120f451a2dc0b5d291a3d4001fbe26ff4fffd5e4
Brian Donald <contact@noimbrian.com>
2019-03-16 17:00:00 -0500

    Moved script to its own file. Refactored containing block

---
+++ static/js/view_formBuilder.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  // Set up image display
+  iw = $('#imageWrapper');
+  iwHeight = iw.css('height');
+  iw.css('width',iwHeight);
+
+
+  // Populate endpoint options
+  $.get('getEndpoints', function(routes){
+    for(route in routes){
+      route = routes[route];
+      newOption = document.createElement('option');
+      newOption.value = route['route'];
+      newOption.textContent = route['description'];
+      $('#endpointSelector').append(newOption);
+      console.log(route);
+      console.log(newOption);
+    }
+  })
+});
