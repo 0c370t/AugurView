bbeaddd12ec72f8cffcb19bcc53cff4f8ad866bc
Brian Donald <contact@noimbrian.com>
2019-03-16 16:59:01 -0500

    Added getEndpoints
    Used for AJAX calls

--- AugurView.py
+++ AugurView.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
-from flask import Blueprint, render_template, current_app
-
+from flask import Blueprint, render_template, current_app, jsonify
+from .. import doc_builder
 view = Blueprint('AugurView',__name__,template_folder="templates",static_folder="static")
 
 
@@ -8,6 +8,17 @@ view = Blueprint('AugurView',__name__,template_folder="templates",static_folder=
 def index():
     return render_template("view/view.html.jinja")
 
+@view.route("/getEndpoints")
+def getEndpoints():
+    docs = doc_builder.endpoint_docs
+    out = []
+    for doc in docs:
+        if doc['method'] == "POST":
+            t = {}
+            t['route'] = doc['route']
+            t['description'] = doc['title']
+            out += [t]
+    return jsonify(out)
 
 # Context Processor
 @view.context_processor
