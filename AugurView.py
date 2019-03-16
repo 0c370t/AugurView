957218b9a94462d91e5564625c8303e153f2b129
Brian Donald <contact@noimbrian.com>
2019-03-16 15:53:08 -0500

    Added state-dependent jQuery
    Added jQuery for both production and development, .min.js will be passed if production, otherwise the full size file is given.

--- AugurView.py
+++ AugurView.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2.7
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 view = Blueprint('AugurView',__name__,template_folder="templates",static_folder="static")
 
@@ -7,3 +7,10 @@ view = Blueprint('AugurView',__name__,template_folder="templates",static_folder=
 @view.route("/")
 def index():
     return render_template("view/template.html.jinja")
+
+
+# Context Processor
+@view.context_processor
+def inject_debug():
+    ## This is used to determine which jQuery file should be loaded by the user
+    return dict(debug=current_app.debug)
