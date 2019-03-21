4f05292f388eb1f829597d1e42b5b4f28f0ca8ca
Brian Donald <contact@noimbrian.com>
2019-03-20 21:24:59 -0500

    Added file uploading and image updating

--- static/js/view_formFunction.js
+++ static/js/view_formFunction.js
@@ -1,3 +1,39 @@
-function formSubmited(){
-  console.log("Es werklik");
+var initialImage = "";
+
+
+
+function uploadFile(){
+  // jQuery doesn't really help me with file inputs, so I have to do it with DOM selector
+  fileInput = document.getElementById("pictureUploadInput");
+  imageDisplay = $("#userImage");
+  if(fileInput.value){
+    file = fileInput.files[0];
+    // File is now available for the upload
+    formData = new FormData();
+    formData.append('file', file);
+    $.ajax({
+      url:"uploadImage",
+      type:"post",
+      data:formData,
+      contentType: false,
+      processData: false,
+      success:uploadFileCallback
+    });
+  } else {
+    alert("Please select an image to use");
+  }
+}
+function uploadFileCallback(data,status){
+  initialImage = data;
+  $("#userImage").attr('src',initialImage);
+}
+function modifyImage(){
+  endpointInput = $("#endpointSelector");
+  imageDisplay = $("#userImage");
+  if(!endpointInput.val()){
+    alert("Please select an operation to use");
+  } else if (initialImage === ""){
+    alert("Please upload an image first");
+  }
+
 }
